d084723e260a133e792317286fb05494aabc29ed
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2016-01-01 16:37:43 -0500

    Fix deps.

--- info.rkt
+++ info.rkt
@@ -1,3 +1,6 @@
 #lang info
 (define scribblings '(("parameter.scrbl" ())))
 (define collection "parameter")
+(define deps '("base"
+               "scheme-lib"))
+(define build-deps '("scribble-lib"))
